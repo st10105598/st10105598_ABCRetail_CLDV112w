06666b0a817f2331dcbeabe7c920755dab4951c5
RC_Student_lab <RC_Student_lab@labG9AEB3>
2025-08-29 13:56:12 +0200

    Add project files.

---
+++ st10105598_ABCRetail_CLDV112w/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace st10105598_ABCRetail_CLDV112w.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
